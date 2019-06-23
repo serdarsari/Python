9b5d3510f364a540a746b8c5f7353eebd44510b7
serdars7 <48322116+serdars7@users.noreply.github.com>
2019-06-23 22:47:24 +0300

    code added

---
+++ Mükemmel Sayı/README.md
@@ -0,0 +1,2 @@
+# Mükemmel Sayı
+Bir sayının kendi hariç bölenlerinin toplamı kendine eşitse bu sayıya "mükemmel sayı" denir. Örnek olarak, 6 mükemmel bir sayıdır. (1 + 2 + 3 = 6)
---
+++ Mükemmel Sayı/mukemmelsayi.py
@@ -0,0 +1,18 @@
+while True:
+
+    girdi=input("Sayı girin : ")
+    
+    if(girdi=="q"):
+        break
+    else:
+        girdi2=int(girdi)
+        toplam=0
+        for i in range(1,girdi2):
+            if (girdi2%i==0):
+                toplam=toplam+i
+            
+        
+        if (toplam==girdi2):
+            print("{} Sayısı, Mükemmel Sayıdır!".format(girdi2))
+        else:
+            print("{} Sayısı, Mükemmel Sayı Değildir!".format(girdi2))
