020859639dad8afe25840f4658669bf62d009726
serdars7 <48322116+serdars7@users.noreply.github.com>
2019-07-14 22:56:50 +0300

    code added

---
+++ Maximum Fonksiyonu/maxfonksiyonu.py
@@ -0,0 +1,11 @@
+#istenilen sayıda sayı girilen ve maximumunu bulan program
+import sys
+def maximum(*girdi):
+    enbuyuk=-sys.maxsize -1           #minimum int değeri... bunu kullanmak için sys'yi import etmemiz gerekiyor
+    for i in girdi:
+        if enbuyuk<i:
+            enbuyuk=i
+    
+    return enbuyuk
+	
+print(maximum(1,2,4,7,3,912,4,3,999))
